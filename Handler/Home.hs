1623ce57eeae6bbebda89ea62143509c2e0e9961
Curtis Gagliardi <cgag@riseup.net>
2014-04-13 17:38:13 -0700

    Working base64 encoded images.  Ditched conduit.

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,10 +2,16 @@
 module Handler.Home where
 
 import Import
-import Data.Conduit
+import Data.Text as T
 import Data.ByteString
+import qualified Data.ByteString.Char8 as BC
+import Data.Conduit
+import Data.Conduit.Binary
 import Data.Text.Encoding (decodeUtf8)
 import qualified Data.ByteString.Base64 as Base64
+import System.Directory
+import System.IO (hClose)
+import System.IO.Temp
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -17,7 +23,7 @@ import qualified Data.ByteString.Base64 as Base64
 getHomeR :: Handler Html
 getHomeR = do
     (formWidget, formEnctype) <- generateFormPost uploadForm
-    let submission  = Nothing :: Maybe (FileInfo, Text)
+    let submission  = Nothing :: Maybe DataURIs
         handlerName = "getHomeR" :: Text
     defaultLayout $ do
         aDomId <- newIdent
@@ -30,11 +36,16 @@ postHomeR = do
     let handlerName = "postHomeR" :: Text
     submission <- (case result of
                       FormSuccess info -> do
-                        bytes <- fileSource info $$ encodeSink
-                        let encoded = decodeUtf8 bytes
-                        return $ Just (info,encoded)
+                          (tmpPath, tmpHandle) <- liftIO $ openTempFile "." "encodeTmp"
+                          liftIO $ hClose tmpHandle 
+                          runResourceT $ fileSource info $$ sinkFile tmpPath
+                          bytes <- liftIO $ BC.readFile tmpPath
+                          liftIO $ removeFile tmpPath
+                          let mime = fileContentType info
+                          let encoded = decodeUtf8 $ Base64.encode bytes
+                          return $ Just $ buildDataURIs mime encoded
                       _ -> do 
-                        return Nothing)
+                          return Nothing)
 
     defaultLayout $ do
         setTitle "Base64 Encoded Data-URI Generator"
@@ -44,9 +55,32 @@ uploadForm :: Form FileInfo
 uploadForm = renderDivs $ fileAFormReq "Or manually select a file:"
 
 {-TODO: pretty sure this could just be awaitForever Base64.encode-}
-encodeSink :: Sink ByteString Handler ByteString
-encodeSink = do
+encodeConduit :: (Monad m) => Conduit ByteString m ByteString
+encodeConduit = do
   file <- await
   case file of
-    Nothing -> return ""
-    Just bytes -> return $ Base64.encode bytes
+    Nothing -> return ()
+    Just bytes -> do
+      yield $ Base64.encode bytes
+      encodeConduit
+
+data DataURIs = DataURIs {
+    imageTag :: Maybe Text
+  , raw :: Text
+}
+
+buildDataURIs :: Text -> Text -> DataURIs
+buildDataURIs mime encoded = DataURIs {
+    raw  = encoded
+  , imageTag = if isImg mime
+               then Just $ dataImgTag mime encoded 
+               else Just "not an image :("
+}
+
+dataImgTag :: Text -> Text -> Text
+dataImgTag mime encoded = "<img src=\"data:" <> mime <> ";base64," <> encoded <> "\" />"
+
+isImg :: Text -> Bool
+isImg mime = case T.split (== '/') mime of
+                (t:_) -> "image" == t
+                _ -> False
